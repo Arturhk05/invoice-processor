e1800bc609922afa867a751772eac70235062512
Arturhk05 <arturhandow@gmail.com>
2026-05-02 01:04:35 -0300

    feat(api-gateway): add helmet middleware

--- services/api-gateway/src/main.ts
+++ services/api-gateway/src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import helmet from 'helmet';
 import { AppModule } from './app.module.js';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter.js';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(helmet());
   app.useGlobalFilters(new HttpExceptionFilter());
 
   const config = app.get(ConfigService);
